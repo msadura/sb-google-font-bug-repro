67954380de6ee7782275f85ff68a2defd569afe6
Marek <m.sadura@outlook.com>
2023-04-25 14:40:22 +0200

    Repro borken google font

---
+++ nextjs/default-ts/.storybook/preview.ts
@@ -0,0 +1,25 @@
+import type { Preview } from '@storybook/react';
+import { PT_Sans_Narrow } from 'next/font/google';
+
+export const font = PT_Sans_Narrow({
+  subsets: ['latin'],
+  variable: '--font-family-serif',
+  fallback: ['Georgia', 'ui-serif', 'serif'],
+  weight: ['400', '700']
+});
+
+console.log('🔥 loaded google font:', font);
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
